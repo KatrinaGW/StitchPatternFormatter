cca4595024114f4207be4279fc67f96fe14a3be2
KatrinaGW <kgwong@ualberta.ca>
2023-04-25 14:37:59 -0600

    clean up tests

--- lib/src/test/java/pattern_printer/PatternFormatterTest.java
+++ lib/src/test/java/pattern_printer/PatternFormatterTest.java
@@ -2,29 +2,45 @@ package pattern_printer;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PatternFormatterTest {
+    StitchType head;
+    StitchType first;
+    StitchType second;
+    StitchType third;
+    StitchType tail;
+    StitchGlossary testPattern;
+
+    @BeforeEach
+    public void setStitches(){
+        head = new StitchType("head", ".");
+        first = new StitchType("a", "!");
+        second = new StitchType("b", "?");
+        third = new StitchType("c", "&");
+        tail = new StitchType("tail", "|");
+
+        testPattern = new StitchGlossary();
+
+        testPattern.addStitchOrThrow(head);
+        testPattern.addStitchOrThrow(first);
+        testPattern.addStitchOrThrow(second);
+        testPattern.addStitchOrThrow(third);
+        testPattern.addStitchOrThrow(tail);
+    }
+
+
     private PatternFormatter getTestPatternFormatter(){
         return new PatternFormatter();
     }
 
     @Test
     public void getPatternStringWithOneRepTest(){
-        StitchType head = new StitchType("head", ".");
-        StitchType first = new StitchType("a", "!");
-        StitchType second = new StitchType("b", "?");
-        StitchType tail = new StitchType("tail", "|");
 
         String testString = "head * a b * 2 tail";
 
         PatternFormatter patternFormatter = getTestPatternFormatter();
-        StitchGlossary testPattern = new StitchGlossary();
-
-        testPattern.addStitchOrThrow(head);
-        testPattern.addStitchOrThrow(first);
-        testPattern.addStitchOrThrow(second);
-        testPattern.addStitchOrThrow(tail);
 
         String result = patternFormatter.getPatternString(testString, testPattern);
 
@@ -33,22 +49,10 @@ public class PatternFormatterTest {
 
     @Test
     public void getPatternStringMultiRepsTest(){
-        StitchType head = new StitchType("head", ".");
-        StitchType first = new StitchType("a", "!");
-        StitchType second = new StitchType("b", "?");
-        StitchType third = new StitchType("c", "&");
-        StitchType tail = new StitchType("tail", "|");
 
         PatternFormatter patternFormatter = getTestPatternFormatter();
-        StitchGlossary testPattern = new StitchGlossary();
 
         String testString = "head b a c turn * a b * c a * 3 turn * 2 b b tail";
-
-        testPattern.addStitchOrThrow(head);
-        testPattern.addStitchOrThrow(first);
-        testPattern.addStitchOrThrow(second);
-        testPattern.addStitchOrThrow(third);
-        testPattern.addStitchOrThrow(tail);
 
         String result = patternFormatter.getPatternString(testString, testPattern);
 
@@ -57,22 +61,10 @@ public class PatternFormatterTest {
 
     @Test
     public void getPatternStringEntireRepetition(){
-        StitchType head = new StitchType("head", ".");
-        StitchType first = new StitchType("a", "!");
-        StitchType second = new StitchType("b", "?");
-        StitchType third = new StitchType("c", "&");
-        StitchType tail = new StitchType("tail", "|");
 
         PatternFormatter patternFormatter = getTestPatternFormatter();
-        StitchGlossary testPattern = new StitchGlossary();
 
         String testString = "* head a b c tail turn * 2";
-
-        testPattern.addStitchOrThrow(head);
-        testPattern.addStitchOrThrow(first);
-        testPattern.addStitchOrThrow(second);
-        testPattern.addStitchOrThrow(third);
-        testPattern.addStitchOrThrow(tail);
 
         String result = patternFormatter.getPatternString(testString, testPattern);
 
@@ -83,13 +75,6 @@ public class PatternFormatterTest {
     public void getPatternStringTest(){
         String testString = "a b turn a";
         PatternFormatter patternFormatter = getTestPatternFormatter();
-        StitchGlossary testPattern = new StitchGlossary();
-
-        StitchType first = new StitchType("a", "!");
-        StitchType second = new StitchType("b", "?");
-
-        testPattern.addStitchOrThrow(first);
-        testPattern.addStitchOrThrow(second);
 
         String result = patternFormatter.getPatternString(testString, testPattern);
 
